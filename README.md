6dd4905d2143a746f8dbe6413b3aa54f1930bf22
Sylvain Faucherand <syl.faucherand@gmail.com>
2017-02-09 17:15:23 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Bot
+
+Sample Slack bot
+
+## Installation
+
+Meh, later...
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/bot](https://hexdocs.pm/bot).
---
+++ lib/bot.ex
@@ -0,0 +1,25 @@
+defmodule Bot do
+  use Slack
+
+  def handle_connect(slack, state) do
+    IO.puts "Connected as #{slack.me.name}"
+    {:ok, state}
+  end
+
+  def handle_event(message = %{type: "message"}, slack, state) do
+    case message.text do
+       "Hi" ->
+         send_message("Hello to you too !!!", message.channel, slack)
+       "Bye" ->
+         send_message("Bye bye !", message.channel, slack)
+      _ ->
+        {:ok, state}
+    end
+
+    {:ok, state}
+  end
+  def handle_event(_, _, state), do: {:ok, state}
+
+end
+
+# {:ok, bot} = Slack.Bot.start_link(Bot, [], Application.get_env(:slack, :api_token))
