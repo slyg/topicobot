cc40996e8149afc4cb304a3991f962c40ca17772
Sylvain Faucherand <syl.faucherand@gmail.com>
2017-02-09 22:29:27 +0000

    Robot reacts on user presence

--- lib/bot.ex
+++ lib/bot.ex
@@ -6,10 +6,17 @@ defmodule Bot do
     {:ok, state}
   end
 
+  def handle_event(message = %{type: "presence_change", presence: "active"}, slack, state) do
+
+    IO.inspect message
+    send_message("Hello !", Slack.Lookups.lookup_direct_message_id(message.user, slack), slack)
+
+    {:ok, state}
+  end
   def handle_event(message = %{type: "message"}, slack, state) do
     case message.text do
        "Hi" ->
-         send_message("Hello to you too !!!", message.channel, slack)
+         send_message("Hope you're doing well :grinning:", message.channel, slack)
        "Bye" ->
          send_message("Bye bye !", message.channel, slack)
       _ ->
