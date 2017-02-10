4e7535c2ab1235fa895c980b9e59d5ee657ee427
Sylvain Faucherand <syl.faucherand@gmail.com>
2017-02-10 23:53:38 +0000

    Remove interactions with buttons

--- lib/topico/Bot.ex
+++ lib/topico/Bot.ex
@@ -3,7 +3,7 @@ defmodule Topico.Bot do
 
   @message_types [
     {~r/(hi|hey|hello|salut)/, :hi},
-    {~r/(yes|yep)/, :yes},
+    {~r/(yes|yep|wfh)/, :yes},
     {~r/(no|nope)/, :no},
     {~r/bye/, :bye}
   ]
@@ -14,8 +14,12 @@ defmodule Topico.Bot do
   end
 
   def handle_event(message = %{type: "presence_change", presence: "active"}, slack, state) do
-    user_dm_channel_id = Slack.Lookups.lookup_direct_message_id(message.user, slack)
-    send_message("Hello :)", user_dm_channel_id, slack)
+    user_dm_channel_id =
+      Slack.Lookups.lookup_direct_message_id(message.user, slack)
+    send_message(
+      "Hi, I hope you're doing well :grinning:, can you tell me if you WFH today ?",
+      user_dm_channel_id, slack
+    )
 
     {:ok, state}
   end
@@ -23,28 +27,8 @@ defmodule Topico.Bot do
     if is_direct_message?(message, slack) do
 
       case reply(message.text) do
-
-        {:ok, response, {:yes, _}} ->
-          send_message(response, message.channel, slack)
-
-        {:ok, response, {:no, _}} ->
-          send_message(response, message.channel, slack)
-
         {:ok, response, _} ->
-          attachments = [
-            %{
-              title: "Are you WFH today ?",
-              callback_id: "123",
-              attachment_type: "default",
-              actions: [
-                %{name: "yes", text: "Yes", value: "yes", type: "button"},
-                %{name: "no", text: "No", value: "no", type: "button"}
-              ]
-            }
-          ] |> JSX.encode!
-
-          Slack.Web.Chat.post_message(message.channel, response, %{ attachments: attachments })
-
+          send_message(response, message.channel, slack)
         _ ->
           {:ok, state}
       end
@@ -57,9 +41,7 @@ defmodule Topico.Bot do
 
   defp is_direct_message?(%{channel: channel}, slack), do: Map.has_key? slack.ims, channel
 
-  defp reply(msg) do
-    msg |> parse |> do_reply
-  end
+  defp reply(msg), do: msg |> parse |> do_reply
 
   defp parse(msg) do
     {_, type} = Enum.find(@message_types, {nil, :unknown}, fn {reg, _type} ->
@@ -68,7 +50,7 @@ defmodule Topico.Bot do
     {type, msg}
   end
 
-  defp do_reply(msg = {:hi, _}),  do: {:ok, "I hope you're doing well :grinning:.", msg}
+  defp do_reply(msg = {:hi, _}),  do: {:ok, "Hey, are you WFH today ?", msg}
   defp do_reply(msg = {:no, _}),  do: {:ok, ":office: I understand you're in the office today, have a good day !", msg}
   defp do_reply(msg = {:yes, _}), do: {:ok, ":house: I understand you're working from home today, enjoy the flow !", msg}
   defp do_reply(msg = {:bye, _}), do: {:ok, "Bye bye !", msg}
