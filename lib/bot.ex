64a86f5ec48394e933e8dc7b52d7d0fbd1de368a
Sylvain Faucherand <syl.faucherand@gmail.com>
2017-02-10 11:52:51 +0000

    Improve messages handling

--- lib/bot.ex
+++ lib/bot.ex
@@ -1,30 +1,47 @@
 defmodule Bot do
   use Slack
 
+  @message_types [{~r/(hi|hey|yup|hello|salut)/, :hi}, {~r/bye/, :bye}]
+
   def handle_connect(slack, state) do
     IO.puts "Connected as #{slack.me.name}"
     {:ok, state}
   end
 
   def handle_event(message = %{type: "presence_change", presence: "active"}, slack, state) do
-
-    IO.inspect message
-    send_message("Hello !", Slack.Lookups.lookup_direct_message_id(message.user, slack), slack)
-
+    user_dm_channel_id = Slack.Lookups.lookup_direct_message_id(message.user, slack)
+    send_message("Hello :)", user_dm_channel_id, slack)
+    
     {:ok, state}
   end
   def handle_event(message = %{type: "message"}, slack, state) do
-    case message.text do
-       "Hi" ->
-         send_message("Hope you're doing well :grinning:", message.channel, slack)
-       "Bye" ->
-         send_message("Bye bye !", message.channel, slack)
-      _ ->
-        {:ok, state}
+    if is_direct_message?(message, slack) do
+      reply = reply(message.text)
+      case reply do
+        {:ok, response} -> send_message(response, message.channel, slack)
+        _ -> {:ok, state}
+      end
     end
 
     {:ok, state}
   end
-  def handle_event(_, _, state), do: {:ok, state}
+  def handle_event(_message, _slack, state), do: {:ok, state}
+
+  defp is_direct_message?(%{channel: channel}, slack), do: Map.has_key? slack.ims, channel
+
+  defp reply(msg) do
+    msg |> parse |> do_reply
+  end
+
+  defp parse(msg) do
+    {_, type} = Enum.find(@message_types, {nil, :unknown}, fn {reg, _type} ->
+       String.match?(msg, reg)
+     end)
+    {type, msg}
+  end
+
+  defp do_reply({:hi, _msg}), do: {:ok, "Hi, I hope you're doing well :grinning:"}
+  defp do_reply({:bye, _msg}), do: {:ok, "Bye bye !"}
+  defp do_reply(_), do: {:ko, nil}
 
 end
